dbecbea422b31c02d399b19fdb8ca2248b09ad15
Shiva Ete <shivaete2005@gmail.com>
2022-06-29 22:47:48 +0530

    Adding Plates Co - POC Changes

---
+++ scripts/cart.js
@@ -0,0 +1,126 @@
+const cartItems = [];
+const cartTotalData = {
+    deliveryCost: 0,
+    orderValue: 0,
+    total: 0,
+    incentiveApplied: false
+};
+
+const getProductById = (productId) => {
+    return new Promise((resolve, reject) => {
+        const xhr = new XMLHttpRequest();
+        xhr.open('GET', './assets/mock-data/products.json', true);
+        xhr.onload = (response) => {
+            const data = JSON.parse(response.currentTarget.response);
+            const selectedProduct = data.products.filter((prod) => prod.id === productId)[0];
+            resolve(selectedProduct);
+        }
+        xhr.onerror = () => reject(xhr.statusText);
+        xhr.send();
+    });
+};
+
+const addToCart = (proudctId) => {
+    getProductById(proudctId)
+        .then(selectedProduct =>{
+            if (selectedProduct) {
+                const productExistIndex = cartItems.findIndex(prod => prod.id === selectedProduct.id);
+                if (productExistIndex !== -1) {
+                    const existingProduct = cartItems[productExistIndex];
+                    existingProduct.quantity++;
+                    existingProduct.price = (existingProduct.price * existingProduct.quantity).toFixed(2);
+                } else {
+                    selectedProduct.quantity = 1;
+                    selectedProduct.price = (selectedProduct.price * selectedProduct.quantity).toFixed(2);
+                    cartItems.push(selectedProduct);
+                }
+                updateCartItems();
+                updateToltal();
+            }
+
+        })
+        .catch(error =>{
+            console.log(error.toString());
+        });
+};
+
+const updateCartItems = () => {
+    const cartItemsListElement = document.getElementById('cart-list');
+    let cartItemMarkup = '';
+    for (let index = 0; index < cartItems.length; index++) {
+        const cartItem = cartItems[index];
+        cartItemMarkup += `<li class="cart-list-item">
+            <img class="thumb" src="${cartItem.imgPath}" />
+            <div class="item-detail">
+                <div class="item-title">${cartItem.name}</div>
+                <div class="items">
+                    <button onClick="removeItem('${cartItem.id}')">Remove</button>
+                    <div class="qty">Quantity: ${cartItem.quantity}</div>
+                    <div class="price">$${cartItem.price}</div>
+                </div>
+            </div>
+        </li>`;
+    }
+    cartItemsListElement.innerHTML = cartItemMarkup;
+}
+
+const updateToltal = () => {
+    cartTotalData.orderValue = 0;
+    for (let index = 0; index < cartItems.length; index++) {
+        const item = cartItems[index];
+        cartTotalData.orderValue = (parseFloat(cartTotalData.orderValue) + parseFloat(item.price)).toFixed(2);
+    }
+    getIncentives.then(incentives => {
+        cartTotalData.incentiveApplied = false;
+        for (let index = 0; index < incentives.length; index++) {
+            const incentive = incentives[index];
+            const orderValue = parseFloat(cartTotalData.orderValue);
+            if (cartTotalData.orderValue > 0 && !cartTotalData.incentiveApplied && orderValue >= incentive.minValue && (incentive.maxValue === 0 || orderValue <= incentive.maxValue )) {
+                cartTotalData.deliveryCost = incentive.deliveryCost;
+                cartTotalData.incentiveApplied = true;
+            }
+        }
+        cartTotalData.total = (parseFloat(cartTotalData.orderValue) + parseFloat(cartTotalData.deliveryCost)).toFixed(2);
+        const orderDetailElement = document.getElementById('order-detail');
+        orderDetailElement.innerHTML = `<div class="deliver-cost">Delivery Cost: $${cartTotalData.deliveryCost}</div>
+        <div class="order-value">Total: $${cartTotalData.total}</div>`;
+    }).catch(error =>{
+        console.log(error.toString());
+    });
+};
+
+const getIncentives = new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', './assets/mock-data/incentives.json', true);
+    xhr.onload = (response) => {
+        const data = JSON.parse(response.currentTarget.response);
+        resolve(data.incentives);
+    }
+    xhr.onerror = () => reject(xhr.statusText);
+    xhr.send();
+});
+
+const removeItem = (itemId) => {
+    const itemIndex = cartItems.findIndex(prod => prod.id === itemId);
+    if (itemIndex !== -1) {
+        cartItems.splice(itemIndex, 1);
+        if (cartItems.length) {
+            updateCartItems();
+            updateToltal();
+        } else {
+            emptyCart();
+        }
+    }
+};
+
+const emptyCart = () => {
+    const cartItemsListElement = document.getElementById('cart-list');
+    cartItemsListElement.innerHTML = '';
+    cartTotalData.deliveryCost = 0;
+    cartTotalData.orderValue = 0;
+    cartTotalData.total = 0;
+    cartTotalData.incentiveApplied = false;
+    const orderDetailElement = document.getElementById('order-detail');
+    orderDetailElement.innerHTML = `<div class="deliver-cost">Delivery Cost: $${cartTotalData.deliveryCost}</div>
+    <div class="order-value">Total: $${cartTotalData.total}</div>`;
+};
---
+++ scripts/products.js
@@ -0,0 +1,36 @@
+const getProducts = new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', '../assets/mock-data/products.json', true);
+    xhr.onload = (response) => resolve(JSON.parse(response.currentTarget.response));
+    xhr.onerror = () => reject(xhr.statusText);
+    xhr.send();
+});
+
+const loadProducts = () => {
+    getProducts
+        .then(data =>{
+            const productsListElement = document.getElementById('products-list');
+            let productsMarkup = '';
+            // console.log(data.products);
+            for (let index = 0; index < data.products.length; index++) {
+                const product = data.products[index];
+                productsMarkup += `<li class="list-item">
+                    <div class="product-title">${product.name}</div>
+                    <img src="${product.imgPath}" />
+                    <div class="controls">
+                        <div class="price">$${product.price}</div>
+                        <button onClick="addToCart('${product.id}')">Add To Cart</button>
+                    </div>
+                </li>`;
+            }
+            productsListElement.innerHTML = productsMarkup;
+        })
+        .catch(error =>{
+            console.log(error.toString());
+        });
+};
+
+
+window.onload = () => {
+    loadProducts();
+};
